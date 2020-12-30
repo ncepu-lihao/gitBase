ae9a001254c93b8dec18b355fb3ace2127f0d8c9
15931246811 <1348402526@qq.com>
2020-12-30 16:45:14 +0800

    dev提交

--- gitbase/src/main/java/Gitbase.java
+++ gitbase/src/main/java/Gitbase.java
@@ -2,5 +2,6 @@ public class Gitbase {
     public static void main(String[] args) {
         System.out.println("coming runing");
         System.out.println("updata1");
+        System.out.println("dev--com");
     }
 }
