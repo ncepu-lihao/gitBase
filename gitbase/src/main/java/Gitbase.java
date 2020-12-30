63686924d8b1bd1a746334024c038180eb6ce248
ncepu-lihao <76768247+ncepu-lihao@users.noreply.github.com>
2020-12-30 17:02:26 +0800

    Update Gitbase.java

--- gitbase/src/main/java/Gitbase.java
+++ gitbase/src/main/java/Gitbase.java
@@ -4,7 +4,7 @@ public class Gitbase {
         System.out.println("updata1");
         System.out.println("dev--com");
         System.out.println("主分支1");
-        System.out.println("分支添加");
+        System.out.println("在线修改更新");
         System.out.println("在线");
  
     }
